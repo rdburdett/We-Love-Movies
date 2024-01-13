30e42e4fd86b604771f8c504b0a6023aa80a1637
Robert Burdett <46675270+rdburdett@users.noreply.github.com>
2024-01-12 23:15:31 -0600

    clean up scripts

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,24 @@
 if (process.env.USER) require("dotenv").config();
-const cors = require("cors");
+const cors = require("cors")
 const express = require("express");
 const app = express();
 
 // Import app routes
-const moviesRouter = require("./routes/movies/movies.router");
-const reivewsRouter = require("./routes/reviews/reviews.router");
+const moviesRouter = require("./routes/movies/movies.router")
+const reivewsRouter = require("./routes/reviews/reviews.router")
 const theatersRouter = require("./routes/theaters/theaters.router");
 
 // Import error routes
 const notFound = require("./errors/notFound");
 const errorHandler = require("./errors/errorHandler");
 
-// Main app
-app.use(cors());
-app.use(express.json());
-app.use("/movies", moviesRouter);
-app.use("/reviews", reivewsRouter);
-app.use("/theaters", theatersRouter);
+
+// Main app //
+app.use(cors())
+app.use(express.json())
+app.use("/movies", moviesRouter)
+app.use("/reviews", reivewsRouter)
+app.use("/theaters", theatersRouter)
 
 // Error handling
 app.use(notFound);
